b0ccb848f5b5fe396ac7f0f6d7abcc89ff2120da
ks9058 <tjdrnr614@naver.com>
2025-06-10 10:30:49 +0900

    Day91

---
+++ Day91/1978.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int ret;
+bool isPrime(int num){
+	if(num<=1) return false;
+	for(int i=2; i*i<=num; i++){
+		if(num%i==0) return false;
+	}
+	return true;
+}
+int main(){
+	cin>>n;
+	for(int i=0; i<n; i++){
+		int num;
+		cin>>num;
+		if(isPrime(num)) ret++;
+	}
+	cout<<ret;
+	return 0;
+}
