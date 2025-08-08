20353b96ce66e838e1c7814aa1e4dfe01aaffe12
ks9058 <tjdrnr614@naver.com>
2025-08-08 16:42:13 +0900

    Day142

---
+++ Day142/10870.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n){
+	if(n==0||n==1) return n;
+		
+    return fib(n-2)+fib(n-1);
+}
+    
+int n;
+int main(){
+	cin>>n;
+	cout<<fib(n);
+	return 0;
+}
