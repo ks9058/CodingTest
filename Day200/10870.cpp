fd943365c9effd221b4c71ee4e4f7e6300c63b98
ks9058 <tjdrnr614@naver.com>
2025-10-16 11:49:52 +0900

    Day200

---
+++ Day200/10870.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long fib(long long n){
+	if(n==0||n==1) return n;
+	
+	return fib(n-1) + fib(n-2);
+}
+
+int main(){
+	long long n;
+	cin>>n;
+	cout<<fib(n);
+	return 0;
+}
